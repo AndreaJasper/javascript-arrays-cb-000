4862a1281a3e539b5b9bf75af7198354e8ed5494
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-10-21 16:33:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,5 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  array.upshift(element)
+  return array.upshift(element)
 }
