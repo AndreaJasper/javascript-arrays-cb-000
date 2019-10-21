c01254db78295f759e565d00526c37ae64acb702
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-10-21 16:47:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop(-1);
-
+  array.pop(-1);
+  return array;
 }
