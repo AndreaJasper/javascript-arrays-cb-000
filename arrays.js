203aee1dfaaa0f55be30443ce609bd5a5ca23f9a
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-10-21 16:22:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray() {
   const candy = ["gummy bears", "starburst"];
   const moreCandy = ["candy corn", ...candy];
-  candy
 }
